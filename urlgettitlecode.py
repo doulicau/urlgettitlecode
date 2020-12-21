0d169c957eb3499fcb1c11d58947b0a33a6836b2
doulicau <38684949+doulicau@users.noreply.github.com>
2020-12-21 13:55:36 +0800

    Update urlgettitlecode.py

--- urlgettitlecode.py
+++ urlgettitlecode.py
@@ -1,66 +1,63 @@
 # -*- coding:utf-8 -*-
-import requests,re
+import re
+import requests
 import urllib3
 import logging
-logging.captureWarnings(True)
 from concurrent.futures import ThreadPoolExecutor
-import argparse
 import time
-#import ssl
+import threading
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-#ssl._create_default_https_context = ssl._create_unverified_context
+logging.captureWarnings(True)
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
-
-def parser_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f","--file",help="指定domain文件")
-    return parser.parse_args()
+ 
+start = time.time()
+lock = threading.Lock()
 
 f = open("result.csv", "a", encoding='utf-8')
 f.write("源地址"+","+"跳转地址"+","+"状态码"+","+"标题"+'\n')
 f = f.close()
 
-start = time.time()
-def getTitle(url):
-    f = open("result.csv", "a", encoding='utf-8')
+#获取url列表
+with open('url.txt', 'r', encoding='utf-8') as f:
+    urls_data = [data.strip().strip('\\') for data in f] 
+#获取状态码、标题
+def get_title(url,timeout=5):
     header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
     }
-
     try:
-        res = requests.get(url, headers=header, verify=False, allow_redirects=True, timeout=10)
+        res = requests.get(url, headers=header, verify=False, allow_redirects=True, timeout=timeout)
         code = res.status_code
     except Exception as error:
         code = "无法访问"
 
-    code1 = str(code)
+    with lock:
+        code1 = str(code)
+        if code1 != "无法访问":
+            try:
+                urllib3.disable_warnings()
+                res = requests.get(url, headers=header, verify=False, allow_redirects=True,timeout=timeout)
+                res.encoding = res.apparent_encoding
+                title = re.findall("(?<=\<title\>)(?:.|\n)+?(?=\<)", res.text, re.IGNORECASE)[0].strip()
+            except :
+                title = "[ ]"
+            print(url+","+res.url+","+code1+","+title)
+            with open("result.csv", "a", encoding='utf-8') as f2:
+                f2.writelines(url+","+res.url+","+code1+","+title+'\n')
 
-    if code1 != "无法访问":
-        try:
-            urllib3.disable_warnings()
-            res = requests.get(url, headers=header, verify=False, allow_redirects=True,timeout=10)
-            res.encoding = res.apparent_encoding
-            title = re.findall("(?<=\<title\>)(?:.|\n)+?(?=\<)", res.text, re.IGNORECASE)[0].strip()
-        except :
-            title = "[ ]"
-        f.write(url+","+res.url+","+code1+","+title+'\n')
-        print(url+","+res.url+","+code1+","+title)
-    else:
-        title = " "
-        f.write(url + "," + " " + "," + code1 + "," + title + '\n')
-        print(url + "," + " " + "," + code1 + "," + title)
-
-    f = f.close()
+        else:
+            title = " "
+            print(url + "," + " " + "," + code1 + "," + title)
+            with open("result.csv", "a", encoding='utf-8') as f2:
+                f2.writelines(url + "," + " " + "," + code1 + "," + title + '\n')
 
 
-a = vars(parser_args())
-file = a['file']
-try:
-    with ThreadPoolExecutor(max_workers=100) as executor:
-        for i in open(file, errors="ignore").readlines():
-            executor.submit(getTitle, i.strip().strip('\\'))
-except:
-    print('-f 指定domain文件')
+#多线程
+with ThreadPoolExecutor(max_workers=50) as executor:
+    for urls in urls_data:
+        executor.submit(
+            get_title, url=urls
+        )
+
 end = time.time()
 print("总耗时:",end - start,"秒")
